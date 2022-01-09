ba1c12bf94fcb36dc1e27f2dc8ca93f20863b20a
Austen Mack <austen.mack@asu.edu>
2022-01-09 10:16:44 -0700

    initial commit

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
 
 function navToggle() {
-  btn.classList.toggle('open')
-  nav.classList.toggle('hidden')
-  document.body.classList.toggle('no-scroll')
+    btn.classList.toggle('open')
+    nav.classList.toggle('hidden')
+    document.body.classList.toggle('no-scroll')
 }
 
 btn.addEventListener('click', navToggle)
